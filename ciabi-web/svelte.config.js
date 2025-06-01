fb2bddf9e3e2044ba54679f3c795e0d52db675d0
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2025-06-01 17:53:55 -0400

    fixed the Me page not being centered

--- ciabi-web/svelte.config.js
+++ ciabi-web/svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	kit: {
 		adapter: adapter({
 			fallback: '404.html',
-			pages: 'build',
+			pages: 'build/',
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
