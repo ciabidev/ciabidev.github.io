50614c8264c87153a78758141803265763a2e3b2
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2025-07-20 18:57:26 -0400

    Updates

---
+++ _app/immutable/nodes/3.CIzm2WSV.js
@@ -0,0 +1,53 @@
+import{f as d,h as g,a as i}from"../chunks/DnOifQUk.js";import"../chunks/uu8PZ83G.js";import{$ as u,c as o,s as t,K as f,b as a}from"../chunks/B8iTBF5f.js";import{M as y}from"../chunks/BxDeynpO.js";import{U as p}from"../chunks/5oPsbTzh.js";var w=d('<meta name="title" content="About/Guide to AO Challenger"/> <meta name="description" content="Terms, about and a list of commands"/> <meta name="keywords" content="arcane odyssey, arcane odyssey bot, arcane odyssey discord bot, ao discord bot, ping, pvp, 1v1, find a 1v1, global pvp, global ping, discord bot, discord bot, arcane odyssey bot"/> <meta name="author" content="wheatwhole"/>',1),A=d('<div class="page-wrapper svelte-1cwtbuw"><div class="main svelte-1cwtbuw"><!> <div class="links svelte-1cwtbuw"><a href="https://ko-fi.com/wheatwhole" target="_blank" class="svelte-1cwtbuw"><img src="/support_me_on_kofi.png" alt="support me on kofi" width="100rem"/></a> <!> <!></div></div></div>');function T(h){const c=`
+# What is AO Challenger?
+
+AO Challenger is an Arcane Odyssey Discord bot that allows you to **ping globally for PvP or find a 1v1**. [ALL of the code is fully free and open source](https://github.com/ciabidev/AO-Challenger).
+
+## How to Use
+
+### For Users
+
+\`/help\` — get a list of commands
+
+\`/findpvp\` — join the queue for a 1v1  
+
+\`/globalpvp ping\` — ping an entire region for PvP
+
+\`/invite\` — invite the bot to your server
+
+\`/upvote\` — upvote bot on top.gg to support it for FREE 💖 
+
+### For Admins
+
+To setup the bot for your server, simply use \`/setup\`  
+\`/help\` — get a list of commands 
+
+\`/globalpvp blockuser \` — block a user from all Challenger-related interactions with your server. Can block users from other servers as well.
+
+\`/globalpvp unblockuser \` — unblock a user from your server
+
+\`/globalpvp listblocked \` — list all blocked users
+
+\`/globalpvp setchannel\` — set the channel where global PvP pings will be sent  
+
+\`/globalpvp setregionalroles\` — assign roles to regions to be pinged
+
+\`/globalpvp sethostrole\` — choose the role that's allowed to use /globalpvp ping (@everyone by default)
+
+\`/invite\` — invite the bot to your server
+
+\`/upvote\` — upvote bot on top.gg to support it for FREE 💖
+
+## Terms and Privacy
+
+[Terms, Ethics, Privacy](https://github.com/ciabidev/AO-Challenger/blob/main/TERMS.md)
+[Ban Appeals](https://github.com/ciabidev/AO-Challenger/blob/main/TERMS.md)
+## Why can't I add the bot / Why is it so slow?
+
+Two possible reasons:
+1. I accidentally made it private  
+2. For the bot to scale larger and be in more servers, I need to verify my identity. Unfortunately, I'm not old enough to verify my identity on Discord (18+)
+3. The Render free plan for the bot has expired
+
+In case any of these have happened, message @wheatwhole_ on Discord IMMEDIATELY.
+`;var e=A();g(b=>{var m=w();u.title="AO Challenger",f(6),i(b,m)});var r=o(e),s=o(r);y(s,{source:c});var l=t(s,2),n=t(o(l),2);p(n,{name:"contact and projects",url:"https://wheatwhole.github.io/",img:"/wheatwhole.webp",urlshort:"my profile"});var v=t(n,2);p(v,{name:"the code",url:"https://github.com/ciabidev/AO-Challenger",urlshort:"github.com/ciabidev/AO-Challenger"}),a(l),a(r),a(e),i(h,e)}export{T as component};
