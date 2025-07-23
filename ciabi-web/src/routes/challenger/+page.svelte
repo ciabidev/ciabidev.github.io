5436708e6b66bf2922b275595a0ac3b9cdcf373f
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2025-07-23 18:52:54 -0400

    Update +page.svelte

--- ciabi-web/src/routes/challenger/+page.svelte
+++ ciabi-web/src/routes/challenger/+page.svelte
@@ -10,14 +10,13 @@ AO Challenger is an Arcane Odyssey Discord bot that allows you to **ping globall
 ## How to Use
 
 ### For Users
+\`/invite\` — invite the bot to your server
 
 \`/help\` — get a list of commands
 
-\`/findpvp\` — join the queue for a 1v1  
+\`/findpvp\` — find a 1v1
 
 \`/globalpvp ping\` — ping an entire region for PvP
-
-\`/invite\` — invite the bot to your server
 
 \`/upvote\` — upvote bot on top.gg to support it for FREE 💖 
 
@@ -26,17 +25,19 @@ AO Challenger is an Arcane Odyssey Discord bot that allows you to **ping globall
 To setup the bot for your server, simply use \`/setup\`  
 \`/help\` — get a list of commands 
 
-\`/globalpvp blockuser \` — block a user from all Challenger-related interactions with your server. Can block users from other servers as well.
+\`/globalpvp settings\` — view the current Global PVP settings and Enable/Disable.
 
-\`/globalpvp unblockuser \` — unblock a user from your server
+\`/globalpvp blockuser\` — block a user from all Challenger-related interactions with your server. Can block users from other servers as well.
 
-\`/globalpvp listblocked \` — list all blocked users
+\`/globalpvp unblockuser\` — unblock a user from your server
 
-\`/globalpvp setchannel\` — set the channel where global PvP pings will be sent  
+\`/globalpvp listblocked\` — list all blocked users
 
-\`/globalpvp setregionalroles\` — assign roles to regions to be pinged
+\`/globalpvp assignregions\` — assign regions to channels and ping roles
 
-\`/globalpvp sethostrole\` — choose the role that's allowed to use /globalpvp ping (@everyone by default)
+\`/globalpvp addhostrole\` — add a role that can host pvp (everyone by default)
+
+\`/globalpvp clearhostroles\` — remove all host roles
 
 \`/invite\` — invite the bot to your server
 
@@ -45,13 +46,21 @@ To setup the bot for your server, simply use \`/setup\`
 ## Terms and Privacy
 
 [Terms, Ethics, Privacy](https://github.com/ciabidev/AO-Challenger/blob/main/TERMS.md)
-[Ban Appeals](https://github.com/ciabidev/AO-Challenger/blob/main/TERMS.md)
+
+If you find a player abusing the bot or breaking Discord TOS, please report them ASAP:
+
+[Ban Appeals and Reports](https://tally.so/forms/3X6yqV/share)
+
+## Support the bot
+If you like the bot and want to support it for *free*, you can upvote it via \`/upvote\`.
+
 ## Why can't I add the bot / Why is it so slow?
 
-Two possible reasons:
+Four possible reasons:
 1. I accidentally made it private  
 2. For the bot to scale larger and be in more servers, I need to verify my identity. Unfortunately, I'm not old enough to verify my identity on Discord (18+)
 3. The Render free plan for the bot has expired
+4. The bot is being rate-limited. This means someone is spamming the bot, or there's just a lot of activity.
 
 In case any of these have happened, message @wheatwhole_ on Discord IMMEDIATELY.
 `;
